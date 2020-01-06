64a12ead0ae1597c58cdebef98ed573c61097c5f
lxl <linxianliang@sinosoft.com.cn>
2020-01-06 20:59:47 +0800

    SpringBoot基本配置

--- 01-Spring-Boot-Start/src/main/java/com/springboot/springbootstart/SpringBootStartApplication.java
+++ 01-Spring-Boot-Start/src/main/java/com/springboot/springbootstart/SpringBootStartApplication.java
@@ -9,5 +9,4 @@ public class SpringBootStartApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStartApplication.class, args);
     }
-
 }
--- 02-Spring-Boot-Config/src/main/java/com/springboot/springbootconfig/SpringBootConfigApplication.java
+++ 02-Spring-Boot-Config/src/main/java/com/springboot/springbootconfig/SpringBootConfigApplication.java
@@ -2,9 +2,17 @@ package com.springboot.springbootconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootConfigApplication {
+
+    @RequestMapping("/hello")
+    String index() {
+        return "hello spring boot";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootConfigApplication.class, args);
