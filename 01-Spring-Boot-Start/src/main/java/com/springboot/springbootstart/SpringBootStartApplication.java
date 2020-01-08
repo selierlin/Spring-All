72abf14089bb3a85a4192088efab55e788efcd33
lxl <linxianliang@sinosoft.com.cn>
2020-01-08 13:23:19 +0800

    开启Spring Boot

--- 01-Spring-Boot-Start/src/main/java/com/springboot/springbootstart/SpringBootStartApplication.java
+++ 01-Spring-Boot-Start/src/main/java/com/springboot/springbootstart/SpringBootStartApplication.java
@@ -2,11 +2,18 @@ package com.springboot.springbootstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootStartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStartApplication.class, args);
+    }
+    @RequestMapping("/hello")
+    String index() {
+        return "hello spring boot";
     }
 }
--- 01-Spring-Boot-Start/src/test/java/com/springboot/springbootstart/SpringBootStartApplicationTests.java
+++ 01-Spring-Boot-Start/src/test/java/com/springboot/springbootstart/SpringBootStartApplicationTests.java
@@ -1,13 +1,22 @@
 package com.springboot.springbootstart;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringBootStartApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        String content = mockMvc.perform(MockMvcRequestBuilders.get("/hello")).andReturn().getResponse().getContentAsString();
+        System.out.println(content);
     }
 
 }
